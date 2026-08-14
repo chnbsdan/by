9f2f3629c6347ec80b57596abfb3dba09b41d018
chnbsdan <chnbsdan@gmail.com>
2026-08-14 21:10:07 +0800

    fix: 强制大图占满全屏

--- src/components/ui/image.vue
+++ src/components/ui/image.vue
@@ -38,30 +38,30 @@ function onLoad() {
 
 <style scoped>
 .ui-image {
-  position: relative;
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 100%;
   overflow: hidden;
 }
 .ui-image__img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;  /* ← 改成 cover */
+  width: 100% !important;
+  height: 100% !important;
+  object-fit: cover !important;
   opacity: 0;
   transition: opacity 0.5s ease;
+  display: block;
 }
 .ui-image__img--loaded {
   opacity: 1;
 }
 .ui-image__placeholder {
   position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
 }
 .ui-image__spinner {
   width: 40px;
