16f6298e7bcab064a4a791cb4a62b678061b0524
chnbsdan <chnbsdan@gmail.com>
2026-08-14 10:27:26 +0800

    大图预览背景透明优化

--- src/App.vue
+++ src/App.vue
@@ -1332,7 +1332,7 @@ html, body { width: 100%; height: 100%; background: var(--bg-primary); font-fami
   position: fixed; 
   top: 0; left: 0; right: 0; bottom: 0; 
   z-index: 2000; 
-  background: rgba(0,0,0,0.05);
+  background: transparent;
   justify-content: center; 
   align-items: center; 
   cursor: default; 
